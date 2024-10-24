1ee41b11af702496e6a559f9c70966cb64c985c6
mostafa <mm3530577@gmail.com>
2024-10-24 16:47:05 +0300

    Initial commit

--- android/app/src/main/kotlin/com/example/habeeb/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/habeeb/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.habeeb
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // منع لقطات الشاشة وتسجيل الشاشة
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WebViewExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class WebViewExample extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  WebViewExampleState createState() => WebViewExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class WebViewExampleState extends State<WebViewExample> {
+  late WebViewController _controller;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack();
+          return Future.value(false); // منع الرجوع الافتراضي للنظام
+        }
+        return Future.value(true); // السماح بالرجوع الافتراضي للنظام إذا لم يكن هناك صفحات سابقة
+      },
+      child: Scaffold(
+        body: WebView(
+          initialUrl: 'https://drhabeb.online/', // استبدل بعنوان موقعك
+          javascriptMode: JavascriptMode.unrestricted,
+          initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+          // إضافة User-Agent مخصص للتطبيق
+          userAgent: 'mostafa',  // استبدل 'YourAppName' باسم يعبر عن التطبيق الخاص بك
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith('https://drhabeb.online/')) {
+              return NavigationDecision.navigate; // فتح الروابط داخل التطبيق
+            } else {
+              _launchURL(request.url); // فتح الروابط الخارجية في المتصفح
+              return NavigationDecision.prevent; // منع الروابط من الفتح داخل التطبيق
+            }
+          },
+          onPageFinished: (String url) {
+            _controller.runJavascript(
+                "document.querySelectorAll('video').forEach(video => video.play());");
+          },
+        ),
       ),
-      body: const WebViewExample(),
     );
   }
-}
 
-class WebViewExample extends StatelessWidget {
-  const WebViewExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const WebView(
-      initialUrl: 'https://drhabeb.online',
-      javascriptMode: JavascriptMode.unrestricted,
-    );
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
